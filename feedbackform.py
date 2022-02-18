2319ff1b8f3a8c3c2fa20e1a3f9d2c572d15b15e
ShankarAnand R <shankaranand4415@gmail.com>
2022-02-18 20:57:13 +0530

    added comments

--- feedbackform.py
+++ feedbackform.py
@@ -1,18 +1,24 @@
 from selenium import webdriver
 option = webdriver.ChromeOptions()
+
+    # other utilities 
 # option.add_argument("-incognito")
 #option.add_argument("--headless")
 #option.add_argument("disable-gpu")
 
-browser = webdriver.Chrome(executable_path="C:\\chromedriver\\chromedriver.exe", options=option)
-browser.get('https://docs.google.com/forms/d/e/1FAIpQLSd53vnmBmug3PMOf2knxGaCVhlnlQqoV9TmCu3ASqlHa_sjUA/viewform')
+browser = webdriver.Chrome(executable_path="<path>", options=option) #example: "C:\\chromedriver\\chromedriver.exe"
+browser.get('<formLink>') # paste your form link here <formLink>
 
 
 radio_buttons = browser.find_elements_by_class_name("appsMaterialWizToggleRadiogroupOffRadio")
-radio_buttons[2].click() # Section-C
+# radio_buttons[2].click() # uncomment if u section is asked
 
-for i in range(5,760,4):  
+for i in range(5,760,4):   # (start,end,review(0,1,2,3))
     radio_buttons[i].click() # feedbacks (good)
+
+
+
+# use below code for other refs
 
 # browser.find_elements_by_class_name("quantumWizTextinputPapertextareaMainContent exportContent")[0].send_keys(".")
 # browser.find_elements_by_class_name("quantumWizTextinputPapertextareaEl modeLight freebirdFormviewerComponentsQuestionTextLong freebirdFormviewerComponentsQuestionTextTextInput freebirdThemedInput")[0].send_keys("..")
